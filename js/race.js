bd15370a8a5613acd9bb1304de5ba277f70420dd
Conor <conormccue@gmail.com>
2019-10-15 11:22:37 -0700

    add style.css

--- js/race.js
+++ js/race.js
@@ -1,22 +1,27 @@
 'use strict';
 //Global Variables
-var targetRaceForm = document.getElementById('raceDiv');
-console.log(targetRaceForm);
-// //travis navigator, matt driver
-// var Race = function(name, raceAbilities, raceBonus, raceSpeed){
-//   this.name = name;
-//   this.raceAbilities = raceAbilities;
-//   this.raceBonus = raceBonus;
-//   this.raceSpeed = raceSpeed;
-// };
+var targetRaceForm = document.getElementById('raceSelector');
+var newRaceNode = document.createElement('option');
+//Object and Array Associated Variables
+var raceArray = [];
+
+// travis navigator, matt driver
+var Race = function(name, raceAbilities, raceBonus, raceSpeed){
+  this.name = name;
+  this.raceAbilities = raceAbilities;
+  this.raceBonus = raceBonus;
+  this.raceSpeed = raceSpeed;
+  raceArray.push(this);
+};
 
 // //empty array is for possible raceAbilities
-// var dwarf = new Race('Dwarf',[],[cons,2],25);
-// var elf = new Race('Elf',[],[dex,2],'30'); //int +1
-// var halfling = new Race('Halfling',[],[dex,2],25);
-// var human = new Race('Human',[],[abilityScore,1],30);
-// var dragonborn = new Race('Dragonborn',[],[str,2],30); //charisma +1
-// var Gnome = new Race('Gnome',[],[cons,1],25);
-// var halfElf = new Race('Half Elf',[],[char,2],30); //two other ability points +1
-// var halfOrc = new Race('Half Orc',[],[cons,1],30);
-// var tiefling = new Race('Tiefling',[],[char,2],30);
+var dwarf = new Race('Dwarf',[],['cons',2],25);
+var elf = new Race('Elf',[],['dex',2],'30'); //int +1
+var halfling = new Race('Halfling',[],['dex',2],25);
+var human = new Race('Human',[],['abilityScore',1],30);
+var dragonborn = new Race('Dragonborn',[],['str',2],30); //charisma +1
+var Gnome = new Race('Gnome',[],['cons',1],25);
+var halfElf = new Race('Half Elf',[],['char',2],30); //two other ability points +1
+var halfOrc = new Race('Half Orc',[],['cons',1],30);
+var tiefling = new Race('Tiefling',[],['char',2],30);
+console.log(raceArray);
