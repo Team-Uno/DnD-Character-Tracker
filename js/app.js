560e461411a34b9538a189a5df77861596767de7
tskyles <travis.skyles@gmail.com>
2019-10-15 09:43:17 -0700

    created ranger class abilities

--- js/app.js
+++ js/app.js
@@ -117,7 +117,116 @@ var rougeAbilities = {
       ],
     },
   },
-},
+};
+
+var rangerAbilities = {
+  generalAbilities: [
+    'favored enemy',
+    'natural explorer',
+    'primeval awareness',
+    'extra attack',
+    'land\'s stride',
+    'hide in plain sight',
+    'vanish',
+    'feral senses',
+    'foe slayer'
+  ],
+  spells: {
+    firstLevel: [
+      'alarm',
+      'animal friendship',
+      'cure wounds',
+      'detect magic',
+      'detect poision and disease',
+      'ensnaring strike',
+      'fog cloud',
+      'goodberry',
+      'hail of thorns',
+      'hunter\'s mark',
+      'jump',
+      'longstrider',
+      'speak with animals'
+    ],
+    secondLevel: [
+      'animal messenger',
+      'bark skin',
+      'beast sense',
+      'cordon of arrows',
+      'dark vision',
+      'find traps',
+      'lesser restoration',
+      'locate animals or plants',
+      'locate objects',
+      'pass without trace',
+      'protection from poison',
+      'silence',
+      'spike growth'
+    ],
+    thirdLevel: [
+      'conjure animals',
+      'conjure barrage',
+      'daylight',
+      'lightning arrow',
+      'nondetection',
+      'plant growth',
+      'protection from energy',
+      'speak with plants',
+      'water breathing',
+      'water walk',
+      'wind wall'
+    ],
+    fourthLevel: [
+      'conjure woodland beings',
+      'freedom of movement',
+      'grasping vine',
+      'locate creature',
+      'stoneskin'
+    ],
+    fifthLevel: [
+      'commune with nature',
+      'conjure volley',
+      'swift quiver',
+      'tree stride',
+    ],
+  },
+  fightingStyle: [
+    'archery',
+    'defense',
+    'dueling',
+    'two-weapon fighting'
+  ],
+  rangerArchetype: {
+    hunter: {
+      huntersPrey: [
+        'colossus slayer',
+        'giant killer',
+        'horder breaker'
+      ],
+      defensiveTactics: [
+        'escape the horde',
+        'multiattack defense',
+        'steel will'
+      ],
+      multiattack: [
+        'volley',
+        'whirlwind attack'
+      ],
+      superiorHuntersDefense: [
+        'evasion',
+        'stand against the tide',
+        'uncanny dodge'
+      ],
+    },
+    beastMaster: {
+      skills: [
+        'ranger\'s companion',
+        'exceptional training',
+        'bestial fury',
+        'share spells',
+      ],
+    },
+  },
+};
 
 var fighterClass = new CharacterClass(
   'fighter',
