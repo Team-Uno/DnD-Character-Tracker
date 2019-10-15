105349744266d3e3cdc6a8d5c2cd14d82ae1e46f
tskyles <travis.skyles@gmail.com>
2019-10-15 09:19:30 -0700

    created CharacterClass constructor

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var CharacterClass = function(name, hitPoints, hitDice, saveThrow, skills, abilities, proficiency, spellsKnown, spells) {
+  this.name = name;
+  this.hitPoints = hitPoints;
+  this.hitDice = hitDice;
+  this.saveThrow - saveThrow;
+  this.skills = skills;
+  this.abilities = abilities;
+  this.proficiency = proficiency;
+  this.spellsKnown = spellsKnown;
+  this.spells = spells;
+};
+
+
+
+
+
+
