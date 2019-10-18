499b937d7ea07e3a6f7a9dc13bad2c03f8377b11
tskyles <travis.skyles@gmail.com>
2019-10-17 23:08:02 -0700

    capitalized class names

--- js/charCreate.js
+++ js/charCreate.js
@@ -259,10 +259,25 @@ var Character = function(name, level, xp, gold, startingHitPoints, skills, abili
   this.characterClass = characterClass;
   this.abilityModifiers = [0, 0, 0, 0, 0, 0];
   this.race = race;
+  this.speed;
   this.classLogo = '';
 
   characterArray.push(this);
 };
+Character.prototype.getSpeed = function(){
+  if(this.characterClass.toLowerCase() === 'fighter'){
+    this.classLogo = '../imgs/fighterLogo.png';
+  }
+  if(this.characterClass.toLowerCase() === 'rouge'){
+    this.classLogo = '../imgs/rogueLogo.png';
+  }
+  if(this.characterClass.toLowerCase() === 'ranger'){
+    this.classLogo = '../imgs/rangerLogo.png';
+  }
+  if(this.characterClass.toLowerCase() === 'wizard'){
+    this.classLogo = '../imgs/wizardLogo.png';
+  }
+}
 
 Character.prototype.calcAbilityModifier = function(){
   for(var i = 0; i < this.abilityScore.length; i++){
@@ -493,11 +508,11 @@ function pushAbilitiesToArray(){
 
 new CharacterClass('Fighter', 10, 6, ['str', 'con'], ['acrobatics', 'animal handling', 'athletics', 'history', 'insight', 'intimidation', 'perception', 'survival'], fighterAbilities, [2, 3, 4, 5, 6]);
 
-new CharacterClass('ranger', 10, 6, ['str', 'dex'], ['animal handling', 'athletics', 'insight', 'investigation', 'nature', 'perception', 'stealth', 'survival'], rangerAbilities, [2, 3, 4, 5, 6]);
+new CharacterClass('Ranger', 10, 6, ['str', 'dex'], ['animal handling', 'athletics', 'insight', 'investigation', 'nature', 'perception', 'stealth', 'survival'], rangerAbilities, [2, 3, 4, 5, 6]);
 
-new CharacterClass('rouge', 8, 5, ['dex', 'int'], ['acrobatics', 'athletics', 'deception', 'insight', 'intimidation', 'perception', 'performance', 'persuasion', 'sleight of hand', 'stealth'], rougeAbilities, [2, 3, 4 ,5, 6]);
+new CharacterClass('Rouge', 8, 5, ['dex', 'int'], ['acrobatics', 'athletics', 'deception', 'insight', 'intimidation', 'perception', 'performance', 'persuasion', 'sleight of hand', 'stealth'], rougeAbilities, [2, 3, 4 ,5, 6]);
 
-new CharacterClass('wizard', 6, 4, ['int', 'wis'], ['arcana', 'history', 'insight','investigation', 'medicine', 'religion'], wizardAbilities, [2, 3, 4, 5, 6]);
+new CharacterClass('Wizard', 6, 4, ['int', 'wis'], ['arcana', 'history', 'insight','investigation', 'medicine', 'religion'], wizardAbilities, [2, 3, 4, 5, 6]);
 
 
 
